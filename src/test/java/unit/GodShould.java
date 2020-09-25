12e3cb050f9f9818e9a19b328fc46708be7b6ad2
Carlos Ble <carlos@leanmind.es>
2020-09-25 15:34:00 +0100

    improving tests

--- src/test/java/unit/GodShould.java
+++ src/test/java/unit/GodShould.java
@@ -17,4 +17,16 @@ public class GodShould {
         God god = new God();
         assertThat(god.substract(2,2)).isEqualTo(0);
     }
+
+    @Test
+    public void say_hello() throws Exception {
+        God god = new God();
+        god.sayHello();
+    }
+
+    @Test
+    public void say_bye() throws Exception {
+        God god = new God();
+        god.sayBye();
+    }
 }
