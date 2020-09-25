06f77eb3e9da08f63fe6258c6695b9a3f5e223f0
Carlos Ble <carlos@leanmind.es>
2020-09-25 15:08:36 +0100

    Improving tests

--- src/test/java/unit/PlayerShould.java
+++ src/test/java/unit/PlayerShould.java
@@ -9,17 +9,17 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class PlayerShould {
     @Test
     public void increase_score_when_dragon_plays() throws Exception {
-        Player player = new Player(Role.Dragon, "WhiteDragon", 0);
+        Player player = new Player(Role.Dragon, "WhiteDragon", 5);
 
         player.play();
 
-        assertThat(player.getScore()).isEqualTo(20);
+        assertThat(player.getScore()).isEqualTo(25);
     }
 
     @Test
     public void sets_the_initial_score() throws Exception {
-        Player player = new Player(Role.Dragon, "WhiteDragon", 0);
+        Player player = new Player(Role.Dragon, "WhiteDragon", 3);
 
-        assertThat(player.getScore()).isEqualTo(0);
+        assertThat(player.getScore()).isEqualTo(3);
     }
 }
