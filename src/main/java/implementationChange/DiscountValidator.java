76b957dbda7915ac8a3501ddbdffd507327cd663
Alberto Morales <kodenix@gmail.com>
2024-03-01 16:41:17 +0100

    Added const

--- src/main/java/implementationChange/DiscountValidator.java
+++ src/main/java/implementationChange/DiscountValidator.java
@@ -1,14 +1,14 @@
 package implementationChange;
 
-import jdk.jshell.spi.ExecutionControl;
-
 public class DiscountValidator {
+
     private final Price price;
     public DiscountValidator(Price price) {
         this.price = price;
     }
 
     public boolean hasDiscountMoreThan100() {
-        return price.value >= 100;
+        final int DISCOUNT_100 = 100;
+        return price.isMoreOrEqualThan(new Price(DISCOUNT_100));
     }
 }
