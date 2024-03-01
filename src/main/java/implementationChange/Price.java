347c0b604a7dc1107bdc1da48f037f0bb24c85b1
Luis Rodríguez <luis.rodriguez@leanmind.es>
2024-03-01 14:02:20 +0100

    feat: create a price with value

--- src/main/java/implementationChange/Price.java
+++ src/main/java/implementationChange/Price.java
@@ -1,9 +1,12 @@
 package implementationChange;
 
 public class Price {
+    public final int value;
+
     public Price(int value) {
         if(value < 0) {
             throw new ArithmeticException("Price cannot be negative");
         }
+        this.value = value;
     }
 }
--- src/test/java/unit/PriceShould.java
+++ src/test/java/unit/PriceShould.java
@@ -1,6 +1,7 @@
 package unit;
 
 import implementationChange.Price;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -9,6 +10,15 @@ public class PriceShould {
     @Test
     public void cannot_be_negative() throws Exception {
         var exception = assertThrows(ArithmeticException.class, () -> new Price(-1));
+
         Assertions.assertEquals("Price cannot be negative", exception.getMessage());
+    }
+
+    @Test
+    public void create_a_price_with_value() throws Exception {
+        var price = new Price(10);
+
+        Assertions.assertNotNull(price);
+        Assert.assertEquals(10, price.value);
     }
 }
